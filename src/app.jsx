beeafbba646ea6fb1d9abec22cd5ac4a2588e928
devmemory <devmemorydh@gmail.com>
2022-07-01 23:34:42 +0900

    .

---
+++ src/app.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import makePdf from 'make_pdf'
+import './app.css'
+
+function App() {
+    const onClick = async (e) => {
+        e.preventDefault()
+        await makePdf.viewWithPdf()
+    }
+
+    return (
+        <div className='div_container'>
+            <div className='div_paper'>
+                <div>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos officiis at iure sapiente maxime provident possimus dolorum eveniet illo nisi ullam, animi sunt nobis, error consequatur quos facere. Perspiciatis, harum.
+                </div>
+            </div>
+            <button onClick={onClick}>pdf로 보기</button>
+        </div>
+    )
+}
+
+export default App
